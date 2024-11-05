71662b91e297b52f54ad6c8724ce9923ba89dc83
igorcosta <78645177+igorcostabr@users.noreply.github.com>
2024-11-05 09:57:38 -0300

    Update QUIZ.py

--- QUIZ.py
+++ QUIZ.py
@@ -1,4 +1,4 @@
-print("Seja bem vindo ao Quiz de Pokemon por Igor Costa")
+print("Seja bem vindo ao Quiz de Pokemon criado por Igor Costa")
 resposta_usuario = input("Quer começar agora? (S/N)")
 
 if resposta_usuario != "S": 
@@ -6,14 +6,14 @@ if resposta_usuario != "S":
 
 pontos = 0
 
-print("Começando...")
-
+print("Começando... \n")
+print("Assinale com a alternativa correta (A,B,C ou D)\n")
 print("Qual o nome do protagonista da série de TV Pokémon? \n (A)Elton \n (B)Ash \n (C)John \n (D)Goku \n")
 resposta_1 = input("Resposta: ")
 
 if resposta_1 == "B":
     print("Resposta Correta!")
-    pontos = pontos + 2
+    pontos = pontos + 1
 else:
     print("Resposta incorreta!")
 
@@ -22,16 +22,16 @@ resposta_2 = input("Resposta: ")
 
 if resposta_2 == "D":
     print("Resposta Correta!")
-    pontos = pontos + 2
+    pontos = pontos + 1
 else:
     print("Resposta incorreta!")
 
-print("Com quantos anos o Ash saiu de casa? \n (A)10 \n (B)18d \n (C)13 \n (D)8 \n")
+print("Com quantos anos o Ash saiu de casa? \n (A)10 \n (B)18 \n (C)13 \n (D)8 \n")
 resposta_3 = input("Resposta: ")
 
 if resposta_3 == "A":
     print("Resposta Correta!")
-    pontos = pontos + 2
+    pontos = pontos + 1
 else:
     print("Resposta incorreta!")
 
@@ -40,7 +40,7 @@ resposta_4 = input("Resposta: ")
 
 if resposta_4 == "C":
     print("Resposta Correta!")
-    pontos = pontos + 2
+    pontos = pontos + 1
 else:
     print("Resposta incorreta!")
 
@@ -49,7 +49,52 @@ resposta_5 = input("Resposta: ")
 
 if resposta_5 == "A":
     print("Resposta Correta!")
-    pontos = pontos + 2
+    pontos = pontos + 1
+else:
+    print("Resposta incorreta!")
+
+print("Qual o nome do pokemon inseto que Ash captura no início de sua jornada? \n (A)Bedril \n (B)Scyter \n (C)Caterpie \n (D)Heracross \n")
+resposta_6 = input("Resposta: ")
+
+if resposta_6 == "C":
+    print("Resposta Correta!")
+    pontos = pontos + 1
+else:
+    print("Resposta incorreta!")
+
+print("De quem é a bicicleta que Ash pega emprestada para fugir dos spearrows? \n (A)Brock \n (B)Policial Jane \n (C)Jessie \n (D)Misty \n")
+resposta_7 = input("Resposta: ")
+
+if resposta_7 == "D":
+    print("Resposta Correta!")
+    pontos = pontos + 1
+else:
+    print("Resposta incorreta!")
+
+print("Qual o nome da enfermeira responsável pelo atendimento no Centro Pokemon? \n (A)Joy \n (B)Mary \n (C)Linda \n (D)Sabrina \n")
+resposta_8 = input("Resposta: ")
+
+if resposta_8 == "A":
+    print("Resposta Correta!")
+    pontos = pontos + 1
+else:
+    print("Resposta incorreta!")
+
+print("Qual o Pokemon Lendário que Ash avista voando no céu, logo no início de sua aventura ? \n (A)Ho-Oh \n (B)Lugia \n (C)Moltres \n (D)Fearow \n")
+resposta_9 = input("Resposta: ")
+
+if resposta_9 == "A":
+    print("Resposta Correta!")
+    pontos = pontos + 1
+else:
+    print("Resposta incorreta!")
+
+print("Qual é a primeira insígnia que o Ash conquista? \n (A)Raio \n (B)Pedra \n (C)Cáscata \n (D)Alma \n")
+resposta_10 = input("Resposta: ")
+
+if resposta_10 == "B":
+    print("Resposta Correta!")
+    pontos = pontos + 1
 else:
     print("Resposta incorreta!")
 
